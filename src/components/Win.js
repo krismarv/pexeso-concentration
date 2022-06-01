6e521e617dc75b9eb0ce6d569884b30571e65fc9
Kristyna Marvalova <k.marvalova@outlook.com>
2022-06-01 16:59:08 +0200

    win

--- src/components/Win.js
+++ src/components/Win.js
@@ -6,7 +6,11 @@ export default function Win (props) {
     return (
         <>
         <div className="win">
-            <img src={confetti}></img>
+            <iframe src="https://embed.lottiefiles.com/animation/94888"></iframe>
+            <div id="win-text">
+                <p id="win-message">You won!</p>
+                <button>New Game</button>
+            </div>
         </div>
         </>
     )
